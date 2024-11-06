bdfd76c79fc47f9cb0a156f2dc5aba74a359e448
Yavuz Yazici <4yavuzyazici@gmail.com>
2024-11-06 07:44:14 +0300

    Error Notification And a Partial Created

---
+++ MyPortfolio/MyPortfolio/Controllers/AdminLayoutController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPortfolio.Controllers
+{
+    public class AdminLayoutController : Controller
+    {
+        public ActionResult Layout()
+        {
+            return View();
+        }
+
+        public ActionResult AdminLayoutHead()
+        {
+            return PartialView();
+        }
+    }
+}
