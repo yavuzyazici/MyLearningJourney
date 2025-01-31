3b7f8d43e0ce76120ec519306a443980531713c2
Yavuz Yazici <4yavuzyazici@gmail.com>
2025-01-31 20:08:10 +0300

    Role CRUD operations compated

---
+++ 05-CentalRentACarProject_dotNetCore/CentalRentACar/Cental.WebUI/Controllers/AdminRoleController.cs
@@ -0,0 +1,59 @@
+﻿using Cental.DtoLayer.RoleDtos;
+using Cental.EntityLayer.Entities;
+using Mapster;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace Cental.WebUI.Controllers
+{
+    [Authorize]
+    public class AdminRoleController(RoleManager<AppRole> _roleManager) : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var roles = _roleManager.Roles.ToList();
+            return View(roles.Adapt<List<ResultRoleDto>>());
+        }
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleDto model)
+        {
+            var role = model.Adapt<AppRole>();
+            var result = await _roleManager.CreateAsync(role);
+
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                    return View("CreateRoleAsync", model);
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public async Task<IActionResult> DeleteRole(int id)
+        {
+            var role = await _roleManager.FindByIdAsync(id.ToString());
+            var result = await _roleManager.DeleteAsync(role);
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                    return View("DeleteRole");
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
