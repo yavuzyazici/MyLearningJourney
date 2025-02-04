e92518710e592155fbd11deb7b085d3f55ad2f3f
Yavuz Yazici <4yavuzyazici@gmail.com>
2025-02-04 22:12:13 +0300

    Team part revised to dynamic with ViewComponent

---
+++ 05-CentalRentACarProject_dotNetCore/CentalRentACar/Cental.WebUI/ViewComponents/Default/_DefaultTeamComponent.cs
@@ -0,0 +1,18 @@
+﻿using Cental.DtoLayer.UserDtos;
+using Cental.EntityLayer.Entities;
+using Mapster;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cental.WebUI.ViewComponents.Default
+{
+    public class _DefaultTeamComponent(UserManager<AppUser> _userManager) : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var managers = await _userManager.GetUsersInRoleAsync("Manager");
+            var dto = managers.TakeLast(4).Adapt<List<ResultUserDto>>();
+            return View(dto);
+        }
+    }
+}
