e32b21b334a934155b37180de8755072be69aba3
Yavuz Yazici <4yavuzyazici@gmail.com>
2025-02-04 21:24:00 +0300

    Manager Area created for Managers

---
+++ 05-CentalRentACarProject_dotNetCore/CentalRentACar/Cental.WebUI/Areas/Manager/ViewComponents/ManagerLayout/_ManagerLayoutNavbarComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cental.WebUI.ViewComponents.AdminLayout
+{
+    public class _ManagerLayoutNavbarComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ 05-CentalRentACarProject_dotNetCore/CentalRentACar/Cental.WebUI/Areas/Manager/ViewComponents/ManagerLayout/_ManagerLayoutSideBarComponent.cs
@@ -0,0 +1,14 @@
+﻿using Cental.EntityLayer.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cental.WebUI.ViewComponents.AdminLayout
+{
+    public class _ManagerLayoutSideBarComponent() : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+           return View();
+        }
+    }
+}
