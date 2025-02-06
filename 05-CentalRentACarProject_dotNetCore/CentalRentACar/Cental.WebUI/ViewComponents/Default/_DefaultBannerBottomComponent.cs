99caab14655bcad5e2f4a620098f4a588b825db6
Yavuz Yazici <4yavuzyazici@gmail.com>
2025-02-06 23:41:20 +0300

    Banner revised to ViewComponent

---
+++ 05-CentalRentACarProject_dotNetCore/CentalRentACar/Cental.WebUI/ViewComponents/Default/_DefaultBannerBottomComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cental.WebUI.ViewComponents.Default
+{
+    public class _DefaultBannerBottomComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
