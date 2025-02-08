723af2e41fd66ed3e404f6cd917194d2209cb24d
Yavuz Yazici <4yavuzyazici@gmail.com>
2025-02-08 11:31:29 +0300

    Dynamic statistic counter moved to ViewComponent

---
+++ 05-CentalRentACarProject_dotNetCore/CentalRentACar/Cental.WebUI/ViewComponents/Default/_DefaultFactComponent.cs
@@ -0,0 +1,23 @@
+﻿using Cental.BusinessLayer.Abstract;
+using Cental.DtoLayer.DashboardDtos;
+using Cental.EntityLayer.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cental.WebUI.ViewComponents.Default
+{
+    public class _DefaultFactComponent(IDashboardService _dashboardService) : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            UIDashboardDto dto = new UIDashboardDto();
+            var rentedCars = _dashboardService.Where<Car>(x => x.Transmission.Contains("s"));
+
+            dto.CarsRented = 9;
+
+            dto.BrandCount = _dashboardService.GetCount<Brand>();
+
+            dto.NumberOfCars = _dashboardService.GetCount<Car>();
+            return View(dto);
+        }
+    }
+}
