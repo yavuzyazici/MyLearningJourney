0a5c08a6f211047163b793df6698eb35e5a401b7
Yavuz Yazici <4yavuzyazici@gmail.com>
2025-02-10 17:41:50 +0300

    Dashboard added and project complated

---
+++ 05-CentalRentACarProject_dotNetCore/CentalRentACar/Cental.DtoLayer/DashboardDtos/AdminDashboardDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cental.DtoLayer.DashboardDtos
+{
+    public class AdminDashboardDto
+    {
+        public int MessageCount { get; set; }
+        public int NonReadedMessageCount { get; set; }
+        public int ReadedMessageCount { get; set; }
+        public int CarCount { get; set; }
+        public int RentedCarCount { get; set; }
+        public int BrandCount { get; set; }
+        public int BranchCount { get; set; }
+        public int UserCount { get; set; }
+        public int ServiceCount { get; set; }
+        public int PendingBookingCount { get; set; }
+        public double AverageCarRating { get; set; }
+        public int Total5StarReview { get; set; }
+    }
+}
---
+++ 05-CentalRentACarProject_dotNetCore/CentalRentACar/Cental.WebUI/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,35 @@
+﻿using Cental.BusinessLayer.Abstract;
+using Cental.DtoLayer.DashboardDtos;
+using Cental.EntityLayer.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cental.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class DashboardController(IDashboardService _dashboardService, UserManager<AppUser> _userManager) : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            AdminDashboardDto adminDashboardDto = new AdminDashboardDto();
+            adminDashboardDto.MessageCount = _dashboardService.GetCount<Message>();
+            adminDashboardDto.NonReadedMessageCount = _dashboardService.Where<Message>(x => x.IsRead == false).Count();
+            adminDashboardDto.CarCount = _dashboardService.GetCount<Car>();
+            adminDashboardDto.RentedCarCount = _dashboardService.Where<Booking>(x => x.BookingStatus == EntityLayer.Enums.BookingStatus.Approved).Count();
+            adminDashboardDto.BrandCount = _dashboardService.GetCount<Brand>();
+            adminDashboardDto.BranchCount = _dashboardService.GetCount<Branch>();
+            adminDashboardDto.UserCount = _userManager.Users.Count();
+            adminDashboardDto.ServiceCount = _dashboardService.GetCount<Service>();
+            adminDashboardDto.PendingBookingCount = _dashboardService.Where<Booking>(x=>x.BookingStatus == EntityLayer.Enums.BookingStatus.PendingApproval).Count();
+            adminDashboardDto.AverageCarRating = _dashboardService.Where<Review>(x => x.CarId >= 0).Average(x => x.Rating);
+            adminDashboardDto.Total5StarReview = _dashboardService.Where<Review>(x => x.Rating == 5).Count();
+
+            return View(adminDashboardDto);
+        }
+    }
+}
