036333d58da16124d0b515497b44b4aa597d2f38
Yavuz Yazici <4yavuzyazici@gmail.com>
2025-02-12 12:39:48 +0300

    BookStore WebAPI project created, Entites created,  Architecture created, Context created other changes in description
    Migration complated, DataAccessLayer Generic system builded.

---
+++ 07-BookStoreAPIProject/BookStore.WebApi/BookStore.DataAccessLayer/Context/BookStoreContext.cs
@@ -0,0 +1,21 @@
+﻿using BookStore.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.DataAccessLayer.Context
+{
+    public class BookStoreContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=YAVUZ\\SQLEXPRESS; database=BookStoreAPIDb; integrated security = true; trustServerCertificate = true");
+        }
+
+        DbSet<Category> Categories { get; set; }
+        DbSet<Product> Products { get; set; }
+    }
+}
