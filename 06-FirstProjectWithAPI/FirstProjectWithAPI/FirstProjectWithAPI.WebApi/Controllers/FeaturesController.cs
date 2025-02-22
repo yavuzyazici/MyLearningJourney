655d271b971fe4f63855252973e296ab69ecfd48
Yavuz Yazici <4yavuzyazici@gmail.com>
2025-02-22 09:55:45 +0300

    FeatureController, MessagesController creted

---
+++ 06-FirstProjectWithAPI/FirstProjectWithAPI/FirstProjectWithAPI.WebApi/Controllers/FeaturesController.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using FirstProjectWithAPI.WebApi.Context;
+using FirstProjectWithAPI.WebApi.Dtos.FeatureDtos;
+using FirstProjectWithAPI.WebApi.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstProjectWithAPI.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeaturesController : Controller
+    {
+        private readonly IMapper _mapper;
+        private readonly ApiContext _context;
+        public FeaturesController(IMapper mapper, ApiContext context)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult FeatureList()
+        {
+            var values = _context.Features.ToList();
+            return Ok(_mapper.Map<List<ResultFeatureDto>>(values));
+        }
+        [HttpPost]
+        public IActionResult CreateFeature(CreateFeatureDto createFeatureDto)
+        {
+            var value = _mapper.Map<Feature>(createFeatureDto);
+            _context.Features.Add(value);
+            _context.SaveChanges();
+            return Ok("Adding process complete succesfully");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteFeature(int id)
+        {
+            var value = _context.Features.Find(id);
+            _context.Features.Remove(value);
+            _context.SaveChanges();
+            return Ok("Deleting process completed succesfully");
+        }
+
+        [HttpGet("GetFeature")]
+        public IActionResult GetFeature(int id)
+        {
+            var value = _context.Features.Find(id);
+            return Ok(_mapper.Map<GetByIdFeatureDto>(value));
+        }
+
+        [HttpPut]
+        public IActionResult UpdateFeature(UpdateFeatureDto updateFeatureDto)
+        {
+            var value = _mapper.Map<Feature>(updateFeatureDto);
+            _context.Features.Update(value);
+            return Ok("Updating process completed succesfully");
+        }
+    }
+}
---
+++ 06-FirstProjectWithAPI/FirstProjectWithAPI/FirstProjectWithAPI.WebApi/Controllers/MessagesController.cs
@@ -0,0 +1,64 @@
+﻿using AutoMapper;
+using FirstProjectWithAPI.WebApi.Context;
+using FirstProjectWithAPI.WebApi.Dtos.FeatureDtos;
+using FirstProjectWithAPI.WebApi.Dtos.MessageDtos;
+using FirstProjectWithAPI.WebApi.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstProjectWithAPI.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly ApiContext _context;
+
+        public MessagesController(ApiContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult MessageList()
+        {
+            var value = _context.Messages.ToList();
+            return Ok(_mapper.Map<List<ResultMessageDto>>(value));
+        }
+
+        [HttpPost]
+        public IActionResult CreateMessage(CreateMessageDto createMessageDto)
+        {
+           var value =  _mapper.Map<Message>(createMessageDto);
+            _context.Messages.Add(value);
+            _context.SaveChanges();
+            return Ok("Message added succesfully");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteMessage(int id)
+        {
+            var value = _context.Messages.Find(id);
+            _context.Messages.Remove(value);
+            _context.SaveChanges();
+            return Ok("Deleting process completed succesfully");
+        }
+        [HttpGet("GetMessage")]
+        public IActionResult GetMessage(int id)
+        {
+            var value = _context.Messages.Find(id);
+            return Ok(_mapper.Map<GetByIdMessageDto>(value));
+        }
+
+        [HttpPut]
+        public IActionResult UpdateFeature(UpdateMessageDto updateMessageDto)
+        {
+            var value = _mapper.Map<Message>(updateMessageDto);
+            _context.Messages.Update(value);
+            _context.SaveChanges();
+            return Ok("Updating process completed succesfully");
+        }
+    }
+}
